620ad5669d9bd7899fc1e34709eb2d620bbb4068
Wesley Brown <wesleybrown@wesleybrown.net>
2020-04-28 14:43:35 -0400

    Ensure Items Are Added To Inventories
    Adding an item to an inventory should put that item in the list of items
    returned by that inventory.

--- src/Sakura/Assets/Inventory/Runtime/Inventory.cs
+++ src/Sakura/Assets/Inventory/Runtime/Inventory.cs
@@ -1,7 +1,40 @@
-﻿namespace Sakura.Inventory
+﻿using System.Collections.Generic;
+
+namespace Sakura.Inventory
 {
+    /// <summary>
+    /// An inventory.
+    /// </summary>
     public sealed class Inventory
     {
+        private readonly List<InventoryItem> items;
 
+        /// <summary>
+        /// Create a new empty inventory.
+        /// </summary>
+        public Inventory()
+        {
+            items = new List<InventoryItem>();
+        }
+
+        /// <summary>
+        /// The list of items this inventory currenlty contains.
+        /// </summary>
+        public IList<InventoryItem> Items
+        {
+            get
+            {
+                return items.AsReadOnly();
+            }
+        }
+
+        /// <summary>
+        /// Add an item to this inventory.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
+        public void AddItem(InventoryItem item)
+        {
+            items.Add(item);
+        }
     }
 }
---
+++ src/Sakura/Assets/Inventory/Tests/Runtime/WhenAddingItemsToAnInventory.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Sakura.Inventory.Tests
+{
+    [TestFixture]
+    sealed class WhenAddingItemsToAnInventory
+	{
+        [Test]
+        public void That_item_is_in_the_list_of_all_items()
+		{
+            var inventory = new Inventory();
+            var potatoSeed = new InventoryItem("Potato Seed");
+            var expectedItemList =
+                new List<InventoryItem>() { new InventoryItem("Potato Seed") };
+            inventory.AddItem(potatoSeed);
+            Assert.That(inventory.Items, Is.EqualTo(expectedItemList));
+		}
+	}
+}
