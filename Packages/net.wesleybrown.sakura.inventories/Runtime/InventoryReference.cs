ad2cea54645434aee5263217d4895af74563e92b
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-05 14:44:19 -0400

    Create ScriptableObject For Referencing Inventories
    This will allow the creation of references to inventories as assets that can
    be dragged around in the Unity editor to be used in scripts that need to
    access an inventory.

---
+++ Packages/net.wesleybrown.sakura.inventories/Runtime/InventoryReference.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Sakura.Inventories.Runtime
+{
+    /// <summary>
+    /// A reference to an inventory.
+    /// </summary>
+    [CreateAssetMenu(fileName = "InventoryReference",
+        menuName = "Inventories/Inventory")]
+    public sealed class InventoryReference : ScriptableObject
+	{
+        private Inventory inventory = null;
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return inventory;
+            }
+        }
+
+        private void OnEnable()
+        {
+            inventory = Inventory.WithCapacityAndEmpty(4);
+        }
+    }
+}
