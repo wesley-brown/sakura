e831c7f83437ab9a10421f1bba0d99e4c5a74cfc
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-12 14:16:34 -0400

    Create Item Templates
    Item templates will be what in-game items are created from. This will allow
    the specification of a base item template and then the creation of many
    distinct items from that template.

--- Packages/net.wesleybrown.sakura.inventories/Runtime/Item.cs
+++ Packages/net.wesleybrown.sakura.inventories/Runtime/Item.cs
@@ -7,10 +7,22 @@ namespace Sakura.Inventories.Runtime
     /// </summary>
     public sealed class Item
 	{
-        private static readonly Item nullItem = new Item("Null Item");
+        private static readonly Item nullItem = new Item("Null Item", null);
 
         private readonly Guid id;
         private readonly string name;
+        private readonly ItemTemplate template;
+
+        /// <summary>
+        /// Create a new item from an item template.
+        /// </summary>
+        /// <returns>
+        /// A new item that is an instance of the given item template.
+        /// </returns>
+        public static Item FromTemplate(ItemTemplate template)
+        {
+            return new Item(template.ExternalItemName);
+        }
 
         /// <summary>
         /// The null item.
@@ -34,6 +46,14 @@ namespace Sakura.Inventories.Runtime
         {
             id = Guid.NewGuid();
             this.name = name;
+            template = null;
+        }
+
+        private Item(string name, ItemTemplate template)
+        {
+            id = Guid.NewGuid();
+            this.name = name;
+            this.template = template;
         }
 
         /// <summary>
@@ -58,6 +78,17 @@ namespace Sakura.Inventories.Runtime
             }
         }
 
+        /// <summary>
+        /// The item template from which this item was created.
+        /// </summary>
+        public ItemTemplate Template
+        {
+            get
+            {
+                return template;
+            }
+        }
+
         public override bool Equals(object obj)
         {
             if ((obj == null) || ! this.GetType().Equals(obj.GetType()))
@@ -78,7 +109,8 @@ namespace Sakura.Inventories.Runtime
 
         public override string ToString()
         {
-            return "<Item: name=" + name + ">";
+            return "<Item: " + " Id=" + Id +  ", Name=" + Name + ", Template=" +
+                Template + ">";
         }
     }
 }
---
+++ Packages/net.wesleybrown.sakura.inventories/Runtime/ItemTemplate.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+namespace Sakura.Inventories.Runtime
+{
+    /// <summary>
+    /// A template from which items are created.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Item Template",
+        menuName = "Inventories/Item Template")]
+    public sealed class ItemTemplate : ScriptableObject
+    {
+        [SerializeField]
+        private string externalItemName;
+        [SerializeField]
+        private string internalItemName;
+        [SerializeField]
+        private string itemDescription;
+        [SerializeField]
+        private string description;
+
+        [SerializeField]
+        private Sprite icon;
+
+        /// <summary>
+        /// The in-game name of items created from this template.
+        /// </summary>
+        public string ExternalItemName
+        {
+            get
+            {
+                return externalItemName;
+            }
+        }
+
+        /// <summary>
+        /// The internal name of items created from this item template.
+        /// </summary>
+        public string InternalItemName
+        {
+            get
+            {
+                return internalItemName;
+            }
+        }
+
+        /// <summary>
+        /// The in-game description of items created from this template.
+        /// </summary>
+        public string ItemDescription
+        {
+            get
+            {
+                return itemDescription;
+            }
+        }
+
+        /// <summary>
+        /// The internal description of this item template.
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+        }
+
+        /// <summary>
+        /// A copy of the sprite that is used as this item's icon.
+        /// </summary>
+        public Sprite Icon
+        {
+            get
+            {
+                return Instantiate(icon);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "<ItemTemplate:" + " name=" + name + ">";
+        }
+    }
+}
