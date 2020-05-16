3912359a3f750ac37f70f05cca5015cee13fc4a8
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-15 20:26:32 -0400

    Display Inventory When Interacting With Plantable Soil
    The inventory screen should be shown when interacting with a plantable soil
    plot so that the player can pick a seed to plant.

---
+++ Assets/InventoryUI/InventoryItemIconDisplayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Sakura.Inventories.Runtime;
+
+namespace Sakura.Runtime.InventoryUI
+{
+    /// <summary>
+    /// Displays the icon of an item in an inventory.
+    /// </summary>
+    [RequireComponent(typeof(Image))]
+    public sealed class InventoryItemIconDisplayer : MonoBehaviour
+    {
+        [SerializeField]
+        private InventoryReference inventoryReference = null;
+
+        [SerializeField]
+        private int slotNumber = 0;
+
+        private Inventory inventory = null;
+        private Item item = Item.NullItem;
+        private Image image = null;
+
+        private void Awake()
+        {
+            inventory = inventoryReference.Inventory;
+            item = inventory.Items[slotNumber];
+            image = GetComponent<Image>();
+
+            if (!item.Equals(Item.NullItem))
+            {
+                image.sprite = item.Template.Icon;
+            }
+        }
+    }
+}
