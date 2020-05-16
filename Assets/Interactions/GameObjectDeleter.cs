f1e7eaf241de3a66ab857e7e76810b87db3b7e92
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-15 20:44:24 -0400

    Add Cancel Button to Seed Selection UI
    Add a cancel butotn the the seed selection UI that is shown when selecting a
    seed to plant in case the player wants to not plant any seed.

---
+++ Assets/Interactions/GameObjectDeleter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Sakura.Interactions
+{
+    /// <summary>
+    /// Causes a game object to delete a given game object.
+    /// </summary>
+    public sealed class GameObjectDeleter : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject otherGameObject = null;
+
+        /// <summary>
+        /// Delete the given game object.
+        /// </summary>
+        public void DeleteGameObject()
+        {
+            Destroy(otherGameObject);
+        }
+    }
+}
