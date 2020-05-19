a3a2f6c2207c672eec0cf66b5f8dcf43968b5e9d
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-19 18:03:22 -0400

    Rewrite InplacePrefabInstantiator
    Give InplacePrefabInstantiators the option to instantiate a given prefab as a
    child of the game object it's attached to.

--- Assets/Interactions/InplacePrefabInstantiator.cs
+++ Assets/Interactions/InplacePrefabInstantiator.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
+using System;
 
 namespace Sakura.Interactions
 {
     /// <summary>
     /// A component that instantiates a prefab at the same place as its
-    /// attached game object when interacted with.
+    /// attached game object.
     /// </summary>
+    /// <remarks>
+    /// Can be configured to instantiate a given prefab as a child of itself.
+    /// </remarks>
     public sealed class InplacePrefabInstantiator : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject prefab = null;
+        [SerializeField] private GameObject prefab = null;
+        [SerializeField] private bool asChild = false;
 
         public void PlacePrefabInplace()
         {
-            Instantiate(prefab, transform.position, transform.rotation);
+            if (asChild == true)
+            {
+                Instantiate(prefab, transform);
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, transform.rotation);
+            }
+        }
+
+        private void Awake()
+        {
+            if (prefab == null)
+            {
+                throw new InvalidOperationException("Prefab must be specified");
+            }
         }
     }
 }
