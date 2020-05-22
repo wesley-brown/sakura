21b072c7a1606d58ac42a7b1f92fb2f814b50a6d
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-21 22:15:25 -0400

    Ensure Globally Instantiated Prefabs Have No Parent

---
+++ Packages/net.wesleybrown.sakura.instantiation/Runtime/GlobalGameObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+
+namespace Sakura.Instantiation
+{
+    /// <summary>
+    /// A game object that has no parent.
+    /// </summary>
+    public sealed class GlobalGameObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab = null;
+
+        /// <summary>
+        /// Make this global game object appear in the current scene.
+        /// </summary>
+        /// <returns></returns>
+        public GameObject AppearInScene()
+        {
+            return Instantiate(prefab);
+        }
+
+        private void Awake()
+        {
+            if (prefab == null)
+            {
+                throw new InvalidOperationException("Prefab must not be null.");
+            }
+        }
+    }
+}
---
+++ Packages/net.wesleybrown.sakura.instantiation/Tests/Runtime/When_instantiating_prefabs.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+
+namespace Sakura.Instantiation.Tests
+{
+    sealed class When_instantiating_prefabs
+    {
+        [TestFixture]
+        sealed class Globally
+        {
+            private static string path = "Packages/" +
+                "net.wesleybrown.sakura.instantiation/" + "Tests/" + "Runtime/";
+            private static string globalGameObjectFilename =
+                "GlobalGameObject.prefab";
+            private static string globalGameObjectPrefabPath =
+                path + globalGameObjectFilename;
+
+            [Test]
+            public void They_have_no_parent()
+            {
+                var globalGameObjectPrefab = AssetDatabase
+                    .LoadAssetAtPath<GameObject>(globalGameObjectPrefabPath);
+                var globalGameObject = globalGameObjectPrefab
+                    .GetComponent<GlobalGameObject>();
+                var newGlobalGameObject = globalGameObject.AppearInScene();
+                Assert.That(
+                    newGlobalGameObject.transform.parent,
+                    Is.EqualTo(null));
+            }
+        }
+    }
+}
