d27017db8f633f50e54289175389e402b7576e43
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-24 23:02:15 -0400

    Rewrite Entities Package
    The Sakura Entities package will operate on the concept of prefabs
    representing "classes" and instances of prefabs representing "instances" of
    those "classes". The scripts on a prefab "class" can be either "constructors",
    which are scripts that initialize the "class" instance, or "methods", which
    are scripts that represent a single behavior of that "class". These "methods"
    can be called by creating Unity events that enable one of these "method"
    script components on a given "instance".

---
+++ Packages/net.wesleybrown.sakura.entities/Runtime/Entity/Entity.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+
+namespace Sakura.Entities
+{
+    /// <summary>
+    /// A reference to a game object in a scene.
+    /// </summary>
+    public sealed class Entity : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject forGameObject = null;
+
+        private GameObject referencedGameObject = null;
+
+        private void Awake()
+        {
+            if (forGameObject == null)
+            {
+                throw new InvalidOperationException(
+                    "'For Game Object' property of "
+                    + gameObject.name
+                    + "'s Entity component "
+                    + "must not be null.");
+            }
+            else
+            {
+                referencedGameObject = forGameObject;
+            }
+        }
+
+        public GameObject GameObject
+        {
+            get
+            {
+                return referencedGameObject;
+            }
+        }
+    }
+}
---
+++ Packages/net.wesleybrown.sakura.entities/Runtime/Entity/Methods/Destroy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+namespace Sakura.Entities
+{
+    /// <summary>
+    /// Causes an entity to destroy itself.
+    /// </summary>
+    [RequireComponent(typeof(Entity))]
+    public sealed class Destroy : MonoBehaviour
+    {
+        private Entity entity = null;
+
+        private void Awake()
+        {
+            entity = GetComponent<Entity>();
+        }
+
+        private void OnEnable()
+        {
+            Destroy(entity.GameObject);
+            enabled = false;
+        }
+    }
+}
