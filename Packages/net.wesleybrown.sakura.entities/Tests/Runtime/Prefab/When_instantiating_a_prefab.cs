260b552d42aaac01df3c9643ade0babb7ae038f4
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-27 12:26:32 -0400

    Refactor Prefab Tests

--- Packages/net.wesleybrown.sakura.entities/Tests/Runtime/Prefab/When_instantiating_a_prefab.cs
+++ Packages/net.wesleybrown.sakura.entities/Tests/Runtime/Prefab/When_instantiating_a_prefab.cs
@@ -6,52 +6,55 @@ using System.Collections;
 
 namespace Sakura.Entities.Methods.Tests
 {
-    internal sealed class When_instantiating_a_prefab
+    internal class When_instantiating_a_prefab
     {
-        [TestFixture]
-        internal sealed class At_the_same_location_as_a_given_entity
+        protected static readonly string testsPath =
+            "Packages/"
+            + "net.wesleybrown.sakura.entities/"
+            + "Tests/"
+            + "Runtime/"
+            + "Prefab/";
+        protected static readonly string testPrefabFilename =
+            "testPrefab.prefab";
+        protected static readonly string testPrefabPath =
+            testsPath
+            + testPrefabFilename;
+        protected static readonly GameObject testPrefabPrefab =
+            AssetDatabase
+            .LoadAssetAtPath<GameObject>(testPrefabPath);
+
+        private Prefab testPrefab;
+        internal class As_a_root_game_object : When_instantiating_a_prefab
         {
-            private static readonly string testsPath =
-                "Packages/"
-                + "net.wesleybrown.sakura.entities/"
-                + "Tests/"
-                + "Runtime/"
-                + "Prefab/";
-            private static readonly string testPrefabFilename =
-                "testPrefab.prefab";
-            private static readonly string testPrefabPath =
-                testsPath
-                + testPrefabFilename;
-            private static readonly GameObject testPrefabPrefab =
-                AssetDatabase
-                .LoadAssetAtPath<GameObject>(testPrefabPath);
-
-            private Prefab testPrefab;
-
-            [SetUp]
-            public void SetUp()
+            [TestFixture]
+            internal sealed class At_the_same_location_as_a_given_entity :
+                As_a_root_game_object
             {
-                testPrefab =
-                    Object
-                    .Instantiate(testPrefabPrefab)
-                    .GetComponent<Prefab>();
-            }
+                [SetUp]
+                public void SetUp()
+                {
+                    testPrefab =
+                        Object
+                        .Instantiate(testPrefabPrefab)
+                        .GetComponent<Prefab>();
+                }
 
-            [UnityTest]
-            public IEnumerator The_instantiated_game_object_appears_at_that_location()
-			{
-                var testPrefabInstantiateAtLocationOfEntity =
-                    testPrefab
-                    .GetComponent<InstantiateAtLocationOfEntity>();
-                testPrefabInstantiateAtLocationOfEntity.enabled = true;
-                yield return null;
-                var instantiatedGameObject =
-                    testPrefabInstantiateAtLocationOfEntity
-                    .InstantiatedGameObject;
-                Assert.That(
-                    instantiatedGameObject.transform.position,
-                    Is.EqualTo(testPrefabInstantiateAtLocationOfEntity.Location));
-			}
+                [UnityTest]
+                public IEnumerator That_game_object_appears_at_that_location()
+                {
+                    var instantiateAtLocationOfEntity =
+                        testPrefab
+                        .GetComponent<InstantiateAtLocationOfEntity>();
+                    instantiateAtLocationOfEntity.enabled = true;
+                    yield return null;
+                    var instantiatedGameObject =
+                        instantiateAtLocationOfEntity
+                        .InstantiatedGameObject;
+                    Assert.That(
+                        instantiatedGameObject.transform.position,
+                        Is.EqualTo(instantiateAtLocationOfEntity.Location));
+                }
+            }
         }
     }
 }
