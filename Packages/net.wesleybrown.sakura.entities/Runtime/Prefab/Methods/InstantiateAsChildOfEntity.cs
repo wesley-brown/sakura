65252f504e3e0887282c471a0953a10a0ad45d4c
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-27 13:39:19 -0400

    Allow Instantiation of Prefabs as Child Game Objects

---
+++ Packages/net.wesleybrown.sakura.entities/Runtime/Prefab/Methods/InstantiateAsChildOfEntity.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+
+namespace Sakura.Entities.Methods
+{
+    /// <summary>
+    /// Causes a prefab to instantiate itself as a child of the given entity.
+    /// </summary>
+    [RequireComponent(typeof(Prefab))]
+    public sealed class InstantiateAsChildOfEntity : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject ofEntity = null;
+
+        private Prefab prefab = null;
+        private Entity entity = null;
+        private GameObject instantiatedGameObject = null;
+
+        private void Awake()
+        {
+            if (ofEntity == null)
+            {
+                throw new InvalidOperationException(
+                    "Parameter "
+                    + "'Of Entity' "
+                    + "of "
+                    + gameObject.name
+                    + ".InstantiateAsChildOfEntity"
+                    + " must not be null.");
+            }
+            else
+            {
+                prefab = GetComponent<Prefab>();
+                entity = ofEntity.GetComponent<Entity>();
+            }
+        }
+
+        public Entity Entity
+        {
+            get
+            {
+                return entity;
+            }
+        }
+
+        public GameObject InstantiatedGameObject
+        {
+            get
+            {
+                return instantiatedGameObject;
+            }
+        }
+
+        private void OnEnable()
+        {
+            instantiatedGameObject = Instantiate(
+                prefab.Asset,
+                entity.GameObject.transform);
+            enabled = false;
+        }
+    }
+}
--- Packages/net.wesleybrown.sakura.entities/Tests/Runtime/Prefab/When_instantiating_a_prefab.cs
+++ Packages/net.wesleybrown.sakura.entities/Tests/Runtime/Prefab/When_instantiating_a_prefab.cs
@@ -6,6 +6,7 @@ using System.Collections;
 
 namespace Sakura.Entities.Methods.Tests
 {
+    [SetUpFixture]
     internal class When_instantiating_a_prefab
     {
         protected static readonly string testsPath =
@@ -24,21 +25,22 @@ namespace Sakura.Entities.Methods.Tests
             .LoadAssetAtPath<GameObject>(testPrefabPath);
 
         private Prefab testPrefab;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            testPrefab =
+                Object
+                .Instantiate(testPrefabPrefab)
+                .GetComponent<Prefab>();
+        }
+
         internal class As_a_root_game_object : When_instantiating_a_prefab
         {
             [TestFixture]
             internal sealed class At_the_same_location_as_a_given_entity :
                 As_a_root_game_object
             {
-                [SetUp]
-                public void SetUp()
-                {
-                    testPrefab =
-                        Object
-                        .Instantiate(testPrefabPrefab)
-                        .GetComponent<Prefab>();
-                }
-
                 [UnityTest]
                 public IEnumerator That_game_object_appears_at_that_location()
                 {
@@ -53,6 +55,31 @@ namespace Sakura.Entities.Methods.Tests
                     Assert.That(
                         instantiatedGameObject.transform.position,
                         Is.EqualTo(instantiateAtLocationOfEntity.Location));
+                }
+            }
+        }
+
+        internal class As_a_child_game_object : When_instantiating_a_prefab
+        {
+            [TestFixture]
+            internal sealed class Of_a_given_entity :
+                As_a_child_game_object
+            {
+                [UnityTest]
+                public IEnumerator It_appears_as_a_child_of_that_entity()
+                {
+                    var instantiateAsChildOfEntity =
+                        testPrefab
+                        .GetComponent<InstantiateAsChildOfEntity>();
+                    instantiateAsChildOfEntity.enabled = true;
+                    yield return null;
+                    var instantiatedGameObject =
+                        instantiateAsChildOfEntity
+                        .InstantiatedGameObject;
+                    Assert.That(
+                        instantiatedGameObject.transform.parent.gameObject,
+                        Is.EqualTo(
+                            instantiateAsChildOfEntity.Entity.GameObject));
                 }
             }
         }
