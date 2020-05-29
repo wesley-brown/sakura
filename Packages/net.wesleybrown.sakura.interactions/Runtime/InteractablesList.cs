54eb62d4a1da71bfdceda1cec6f02cc25ec0b7c0
Wesley Brown <wesleybrown@wesleybrown.net>
2020-05-28 20:12:55 -0400

    Create Interactions Package
    This package will contain everything needed for the Sakura Interactions
    subsystem.

---
+++ Packages/net.wesleybrown.sakura.interactions/Runtime/InteractablesList.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Sakura.Entities;
+
+namespace Sakura.Interactions
+{
+    public sealed class InteractablesList : MonoBehaviour
+    {
+        [SerializeField]
+        private Entity forInteractor = null;
+
+        public Entity Interactor
+        {
+            get
+            {
+                return forInteractor;
+            }
+        }
+    }
+}
