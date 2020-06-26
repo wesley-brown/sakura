7ef7df7682bd6c067de6d6f184e94ba5c8d2bb44
Wesley Brown <wesleybrown@wesleybrown.net>
2020-06-26 16:00:47 -0400

    Modify Shiping Bin UI
    Modify the shipping bin UI by adding labels to the sections for which items
    are kept and sold. This will make it clear to the player which items will be
    kept vs which will be sold.

---
+++ Assets/ShippingBinUI/ItemButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Sakura.Inventories.Runtime;
+
+namespace Sakura
+{
+    public sealed class ItemButton : MonoBehaviour
+    {
+        [SerializeField]
+        private int forSlot = 0;
+        private int slot = 0;
+        [SerializeField]
+        [Tooltip("The inventory from which to access the given slot of.")]
+        private InventoryReference of = null;
+        private Inventory inventory = null;
+        private Sprite icon = null;
+        private Image image = null;
+
+        private void Awake()
+        {
+            slot = forSlot;
+            inventory = of.Inventory;
+            icon = inventory.Items[slot].Template.Icon;
+            image = GetComponentInChildren<Image>();
+            image.sprite = icon;
+        }
+    }
+}
