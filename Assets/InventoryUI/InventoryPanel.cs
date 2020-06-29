f29151f9a7fc82d5151d0cda48d1d210df379377
Wesley Brown <wesleybrown@wesleybrown.net>
2020-06-29 14:30:39 -0400

    Modify Shipping Bin UI
    Modify the shipping bin UI to use two instances of the InventoryPanel prefab,
    one for each of the sell and keep sections. This will allow for the sell and
    keep sections to be set to displaying different inventories. The sell section
    will display the shipping bin's inventory while the keep section will display
    the player's inventory.

---
+++ Assets/InventoryUI/InventoryPanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Sakura.Inventories.Runtime;
+
+namespace Sakura.UI
+{
+    public sealed class InventoryPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private InventoryReference inventoryReference = null;
+
+        private Inventory inventory = null;
+
+        private void Awake()
+        {
+            inventory = inventoryReference.Inventory;
+        }
+    }
+}
