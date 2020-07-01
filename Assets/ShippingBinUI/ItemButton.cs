c61117b1f41e5db1cf94bbcc8ed9625a5ef8de9d
Wesley Brown <wesleybrown@wesleybrown.net>
2020-06-30 20:17:58 -0400

    Modify Inventory Panel
    Modify the inventory panel to be given a reference to an inventory which will
    then be used by any item button children that inventory panel has.

--- Assets/ShippingBinUI/ItemButton.cs
+++ Assets/ShippingBinUI/ItemButton.cs
@@ -7,25 +7,50 @@ namespace Sakura
     /// <summary>
     /// A 
     /// </summary>
+    [RequireComponent(typeof(InventoryVariable))]
+    [RequireComponent(typeof(Slot))]
     public sealed class ItemButton : MonoBehaviour
     {
-        [SerializeField]
-        private Slot @for = null;
-
-        [SerializeField]
-        [Tooltip("The inventory from which to access the given slot of.")]
-        private InventoryReference of = null;
-
-        private Inventory inventory = null;
+        private InventoryVariable inventoryVariable = null;
+        private Slot slot = null;
         private Sprite icon = null;
         private Image image = null;
 
         private void Awake()
         {
-            inventory = of.Inventory;
-            icon = inventory.Items[@for.Number].Template.Icon;
+            inventoryVariable = GetComponent<InventoryVariable>();
+            slot = GetComponent<Slot>();
             image = GetComponentInChildren<Image>();
             image.sprite = icon;
+        }
+
+        private void Start()
+        {
+            UpdateSprite();
+        }
+
+        private void Update()
+        {
+            UpdateSprite();
+        }
+
+        public void RemoveItemFromSlot()
+        {
+            inventoryVariable.Inventory.RemoveItemFromSlot(slot.Number);
+            //UpdateSprite();
+        }
+
+        private void UpdateSprite()
+        {
+            var item = inventoryVariable.Inventory.Items[slot.Number];
+            if (item.Equals(Item.NullItem))
+            {
+                image.sprite = null;
+            }
+            else
+            {
+                image.sprite = item.Template.Icon;
+            }
         }
     }
 }
---
+++ Packages/net.wesleybrown.sakura.inventories/Runtime/InventoryVariable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Sakura.Inventories.Runtime
+{
+    public sealed class InventoryVariable : MonoBehaviour
+    {
+        [SerializeField]
+        private InventoryReference @for = null;
+        [SerializeField]
+        private InventoryVariable referencing = null;
+
+        private Inventory inventory = null;
+
+        private void Awake()
+        {
+            if (@for != null)
+            {
+                inventory = @for.Inventory;
+            }
+            else
+            {
+                inventory = referencing.Inventory;
+            }
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return inventory;
+            }
+        }
+
+        public void RemoveItemFrom(Slot slot)
+        {
+            inventory.RemoveItemFromSlot(slot.Number);
+        }
+    }
+}
