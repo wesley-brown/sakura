efe76e6fd38075f2634be73789d47159257496a2
Wesley Brown <wesleybrown@wesleybrown.net>
2020-07-11 11:20:58 -0400

    Create Replacement For Inventory Panel
    NewInventoryPanel will eventually replace InventoryPanel.

---
+++ Assets/InventoryUI/NewInventoryUIViewController.cs
@@ -0,0 +1,24 @@
+﻿using Sakura.Inventories.Runtime;
+using UnityEngine;
+
+namespace Sakura.InventoryUI
+{
+    public sealed class NewInventoryUIViewController : MonoBehaviour
+    {
+        [SerializeField]
+        private InventoryReference inventoryReference = null;
+        [SerializeField]
+        private GameObject buttonPrefab = null;
+
+        private Inventory inventory = null;
+
+        private void Awake()
+        {
+            inventory = inventoryReference.Inventory;
+            for (var i = 0; i < inventory.Capacity; ++i)
+            {
+                Instantiate(buttonPrefab, transform);
+            }
+        }
+    }
+}
