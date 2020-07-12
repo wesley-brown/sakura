e3e302a45d10b88478c2ee7f700dc4d10621821b
Wesley Brown <wesleybrown@wesleybrown.net>
2020-07-11 21:18:28 -0400

    Create New Shipping Bin UI
    This new shipping bin UI will replace the old one.

--- Assets/InventoryUI/NewInventoryUIViewController.cs
+++ Assets/InventoryUI/NewInventoryUIViewController.cs
@@ -11,15 +11,25 @@ namespace Sakura.InventoryUI
         [SerializeField]
         private GameObject buttonPrefab = null;
 
+        private ScrollRect scrollRect = null;
         private Inventory inventory = null;
+
+        public ScrollRect ScrollRect
+        {
+            get
+            {
+                return scrollRect;
+            }
+        }
 
         private void Awake()
         {
-            var scrollRect = GetComponentInChildren<ScrollRect>();
+            scrollRect = GetComponentInChildren<ScrollRect>();
             inventory = inventoryReference.Inventory;
             for (var i = 0; i < inventory.Capacity; ++i)
             {
-                Instantiate(buttonPrefab, scrollRect.content);
+                var button = Instantiate(buttonPrefab, scrollRect.content);
+                button.SetActive(true);
             }
         }
     }
---
+++ Assets/ShippingBinUI/NewShippingBinUIViewController.cs
@@ -0,0 +1,19 @@
+﻿using Sakura.InventoryUI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sakura
+{
+    public sealed class NewShippingBinUIViewController : MonoBehaviour
+    {
+        [SerializeField]
+        private NewInventoryUIViewController sellPanel;
+        [SerializeField]
+        private NewInventoryUIViewController keepPanel;
+
+        public void SellItem(Button sender)
+        {
+            var slotNumber = sender.transform.GetSiblingIndex();
+        }
+    }
+}
