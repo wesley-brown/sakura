4b7a1e044ee0040e02f57c39d7042a4beb851eb7
Wesley Brown <wesleybrown@wesleybrown.net>
2020-07-16 13:18:16 -0400

    Create Wallet Panel Prefab

---
+++ Assets/Economy/WalletPanel.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Sakura
+{
+    /// <summary>
+    /// Displays a wallet's balance.
+    /// </summary>
+    public sealed class WalletPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private WalletReference walletReference = null;
+
+        private Wallet wallet = null;
+        private TextMeshProUGUI textBox = null;
+
+        private void Awake()
+        {
+            textBox = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        private void Start()
+        {
+            wallet = walletReference.Wallet;
+            textBox.text = wallet.Balance.ToString();
+        }
+    }
+}
