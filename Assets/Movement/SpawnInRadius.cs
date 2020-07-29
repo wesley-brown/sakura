5e892716680faf3e0e557ffb35c33dd9e63fda1f
Wesley Brown <wesleybrown@wesleybrown.net>
2020-07-28 22:15:11 -0400

    Spawn Potatoes When Harvesting Them

---
+++ Assets/Movement/SpawnInRadius.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Sakura.Movement
+{
+    public sealed class SpawnInRadius : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab = null;
+
+        public void Spawn()
+        {
+            var location = Random.insideUnitSphere * 2;
+            location += gameObject.transform.position;
+            location.y = gameObject.transform.position.y * 2;
+            Instantiate(prefab, location, Quaternion.identity);
+            Debug.Log("spawned at " + location);
+        }
+    }
+}
