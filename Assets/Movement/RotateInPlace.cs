6049c4431bf1ab4d52bd81b4ca74ce32ea3d1bad
Wesley Brown <wesleybrown@wesleybrown.net>
2020-07-29 12:55:42 -0400

    Cause Potatoes to Rotate in Place

---
+++ Assets/Movement/RotateInPlace.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Sakura.Movement
+{
+    /// <summary>
+    /// Causes a game object to rotate in place.
+    /// </summary>
+    [RequireComponent(typeof(Transform))]
+    public sealed class RotateInPlace : MonoBehaviour
+    {
+        [SerializeField] private Vector3 rotationAngle = Vector3.zero;
+
+        private void Update()
+        {
+            if (rotationAngle != Vector3.zero)
+            {
+                transform.Rotate(rotationAngle.x, rotationAngle.y, rotationAngle.z);
+            }
+        }
+    }
+}
