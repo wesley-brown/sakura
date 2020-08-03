833036045311e8068eb65697ebf7f772b6771b15
Wesley Brown <wesleybrown@wesleybrown.net>
2020-08-03 19:10:43 -0400

    Create ControllableModel Type
    This type will be used by game objects that interpolate the position of an
    entity and are also controlled by the player.

---
+++ Assets/UnityComponents/Rendering/ControllableModel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Sakura.UnityComponents.Rendering
+{
+    [RequireComponent(typeof(UnityModel))]
+    [RequireComponent(typeof(Transform))]
+    public sealed class ControllableModel : MonoBehaviour
+    {
+        private UnityModel unityModel = null;
+
+        private void Awake()
+        {
+            unityModel = GetComponent<UnityModel>();
+        }
+
+        private void Update()
+        {
+            var entity = unityModel.Entity;
+            var interpolatedPosition = Vector3.Lerp(transform.position, entity.Location, Time.deltaTime);
+            if (Vector3.Distance(entity.Location, interpolatedPosition) > 0.1)
+            {
+                transform.position = interpolatedPosition;
+            }
+            else
+            {
+                transform.position = entity.Location;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Destroy(unityModel);
+        }
+    }
+}
