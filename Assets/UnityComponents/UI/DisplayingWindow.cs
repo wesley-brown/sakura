f7d635a4c465c538629eccb4fd17413d20e45c5d
Wesley Brown <wesleybrown@wesleybrown.net>
2020-08-10 17:51:42 -0400

    Create New Plantable Plot Prefab
    This new prefab will be a variant of the plantable plots that shows the seed
    selection UI.

---
+++ Assets/UnityComponents/UI/DisplayingWindow.cs
@@ -0,0 +1,15 @@
+﻿using Sakura.UI;
+using UnityEngine;
+
+namespace Sakura.UnityComponents.UI
+{
+    public sealed class DisplayingWindow : MonoBehaviour
+    {
+        [SerializeField] private GameObject window = null;
+
+        private void Start()
+        {
+            Instantiate(window);
+        }
+    }
+}
