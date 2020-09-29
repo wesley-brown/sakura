6089d9dd1117d3f11d6749e0abaef9f6359f3d3a
Wesley Brown <wesleybrown@wesleybrown.net>
2020-09-29 15:23:20 -0400

    Create DestroyOnInteraction Component
    This component will destroy its game object after it is successfully
    interacted with.

---
+++ Assets/Events/Interactions/DestroyOnInteraction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Sakura.Events.Interactions
+{
+    public sealed class DestroyOnInteraction :
+        MonoBehaviour,
+        SuccessfulInteractionReceiver
+    {
+        public void Receive()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
