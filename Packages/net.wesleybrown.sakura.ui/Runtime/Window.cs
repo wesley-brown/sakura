154fecf43c27ddbff596a1c62ad0f13465be06a9
Wesley Brown <wesleybrown@wesleybrown.net>
2020-10-26 13:13:02 -0400

    Ensure Closed Windows Have Their Game Objects Destroyed
    Closing a window should destroy its game object.

---
+++ Packages/net.wesleybrown.sakura.ui/Runtime/Window.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Sakura.UI
+{
+    /// <summary>
+    /// Represents an independent portion of a screen and its contents.
+    /// </summary>
+    public sealed class Window : MonoBehaviour
+    {
+        /// <summary>
+        /// Close this window.
+        /// </summary>
+        public void Close()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Packages/net.wesleybrown.sakura.ui/Tests/Runtime/When_closing_a_window.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Sakura.UI.Tests
+{
+    sealed class When_closing_a_window
+    {
+        [UnityTest]
+        public IEnumerator Its_game_object_is_destroyed()
+        {
+            var inventoryScreen = new GameObject("Inventory Screen");
+            var window = inventoryScreen.AddComponent<Window>();
+            window.Close();
+            yield return null;
+            Assert.That(inventoryScreen == null);
+        }
+    }
+}
