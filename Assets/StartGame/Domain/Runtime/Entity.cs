72b373f7da217901c07bb684829cf8427385efdd
Wesley Brown <wesleybrown@wesleybrown.net>
2020-11-01 04:57:18 -0500

    Ensure Entities Can Move
    Entities should be able to be moved along a given displacement vector.

---
+++ Assets/StartGame/Domain/Runtime/Entity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Sakura.StartGame.Domain
+{
+    /// <summary>
+    /// A thing in a simulation.
+    /// </summary>
+    public sealed class Entity : MonoBehaviour
+    {
+        private Vector3 location = Vector3.zero;
+
+        /// <summary>
+        /// Where this entity is located.
+        /// </summary>
+        public Vector3 Location
+        {
+            get
+            {
+                return location;
+            }
+        }
+
+        /// <summary>
+        /// Move this entity along a given displacement vector.
+        /// </summary>
+        /// <param name="displacement">
+        /// The displacement vector to move along.
+        /// </param>
+        public void Move(Vector3 displacement)
+        {
+            location += displacement;
+        }
+    }
+}
---
+++ Assets/StartGame/Domain/Tests/Runtime/EntitySpec.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Sakura.StartGame.Domain.Tests
+{
+    sealed class EntitySpec
+    {
+        [Test]
+        public void Entities_are_moved_a_given_amount()
+        {
+            var player = new GameObject("Player");
+            var entity = player.AddComponent<Entity>();
+            entity.Move(new Vector3(1.0f, 0.0f, 0.0f));
+            Assert.That(
+                entity.Location,
+                Is.EqualTo(new Vector3(1.0f, 0.0f, 0.0f)));
+        }
+    }
+}
