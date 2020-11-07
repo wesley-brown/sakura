2cb16df2cb9160c305a90c618930bef5f3cfcfa7
Wesley Brown <wesleybrown@wesleybrown.net>
2020-11-06 22:16:28 -0500

    Create simulation type
    A simulation will approximately imitate a Sakura world. In order to do so,
    there must be a way to include entities in a simulation. These included
    entities will appear in the list of all entities a simulation has.

---
+++ Assets/StartGame/Domain/Runtime/Simulation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Sakura.StartGame.Domain
+{
+    /// <summary>
+    /// An approximate imitation of the world of Sakura.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Simulation", menuName = "Simulation")]
+    public sealed class Simulation : ScriptableObject
+    {
+        private List<Entity> entities = null;
+
+        private void Awake()
+        {
+            entities = new List<Entity>();
+        }
+
+        /// <summary>
+        /// The entities in this simulation.
+        /// </summary>
+        public IEnumerable<Entity> Entities
+        {
+            get
+            {
+                return new List<Entity>(entities);
+            }
+        }
+
+        /// <summary>
+        /// Include a given entity in this simulation.
+        /// </summary>
+        /// <param name="entity">The entity to include.</param>
+        public void Include(Entity entity)
+        {
+            entities.Add(entity);
+        }
+    }
+}
---
+++ Assets/StartGame/Domain/Tests/Runtime/SimulationSpec.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Sakura.StartGame.Domain.Tests
+{
+    sealed class SimulationSpec
+    {
+        [UnityTest]
+        public IEnumerator Included_entities_appear_in_the_list_of_all_entities()
+        {
+            var simulation = ScriptableObject.CreateInstance<Simulation>();
+            var player = new GameObject("Player");
+            var playerEntity = player.AddComponent<Entity>();
+            simulation.Include(playerEntity);
+            yield return null;
+            Assert.That(simulation.Entities, Does.Contain(playerEntity));
+        }
+    }
+}
