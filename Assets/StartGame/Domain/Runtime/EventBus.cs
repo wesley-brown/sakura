d331f90af9896665f2e681ddedb1e9167b573acb
Wesley Brown <wesleybrown@wesleybrown.net>
2020-11-15 15:09:05 -0500

    Implement successful entity registration
    Registering an unregistred entity should succeed since it has not already
    been registered.

---
+++ Assets/StartGame/Domain/Runtime/EventBus.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Sakura.StartGame.Domain
+{
+    public sealed class EventBus : ScriptableObject
+    {
+        [SerializeField] private Simulation simulation = null;
+
+        public bool Register(Entity entity)
+        {
+            return true;
+        }
+    }
+}
---
+++ Assets/StartGame/Domain/Tests/Runtime/EventBusSpec.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Sakura.StartGame.Domain.Tests
+{
+    sealed class EventBusSpec
+    {
+        [Test]
+        public void Registering_an_unregistered_entity_succeeds()
+        {
+            var eventBus = ScriptableObject.CreateInstance<EventBus>();
+            var player = new GameObject("Player");
+            var playerEntity = player.AddComponent<Entity>();
+            var playerWasRegistered = eventBus.Register(playerEntity);
+            Assert.That(playerWasRegistered, Is.True);
+        }
+    }
+}
