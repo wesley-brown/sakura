4009d2b1129375757f96a9014a7430ba44339e2e
Wesley Brown <wesleybrown@wesleybrown.net>
2020-12-01 07:51:31 -0500

    Define an invalid window manager
    A window manager without an event bus is invalid.

---
+++ Assets/StartGame/UI/Runtime/WindowManager.cs
@@ -0,0 +1,24 @@
+﻿using Sakura.StartGame.Domain;
+using System;
+using UnityEngine;
+
+namespace Sakura.StartGame.UI
+{
+    /// <summary>
+    /// Controls the placement and appearance of windows.
+    /// </summary>
+    public sealed class WindowManager : MonoBehaviour
+    {
+        [Header("Initialization")]
+        public EventBus communicatesWith = null;
+
+        private void Start()
+        {
+            if (!communicatesWith)
+            {
+                throw new InvalidOperationException(
+                    "communicatesWith must not be null");
+            }
+        }
+    }
+}
---
+++ Assets/StartGame/UI/Tests/Runtime/WindowManagerSpec.cs
@@ -0,0 +1,29 @@
+﻿using Sakura.StartGame.Domain;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Sakura.StartGame.UI.Tests
+{
+    public sealed class WindowManagerSpec
+    {
+        [UnityTest]
+        public IEnumerator Window_managers_created_without_an_event_bus_are_invalid()
+        {
+            var simulation = ScriptableObject.CreateInstance<Simulation>();
+            var eventBusGameObject = new GameObject("Event Bus");
+            var eventBus = eventBusGameObject.AddComponent<EventBus>();
+            eventBus.forSimulation = simulation;
+            var windowManagerGameObject = new GameObject("Window Manager");
+            var windowManager = windowManagerGameObject
+                .AddComponent<WindowManager>();
+            yield return null;
+            LogAssert.Expect(
+                LogType.Exception,
+                "InvalidOperationException: communicatesWith must not be null");
+            Object.Destroy(simulation);
+            Object.Destroy(eventBusGameObject);
+            Object.Destroy(windowManagerGameObject);
+        }
+    }
+}
