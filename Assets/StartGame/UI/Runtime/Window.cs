8ad6adce0f0001aac5dfb2867933b3f43b852c83
Wesley Brown <wesleybrown@wesleybrown.net>
2020-11-30 22:42:22 -0500

    Define an invalid window
    A window without an event bus is invalid.

---
+++ Assets/StartGame/UI/Runtime/Window.cs
@@ -0,0 +1,24 @@
+﻿using Sakura.StartGame.Domain;
+using System;
+using UnityEngine;
+
+namespace Sakura.StartGame.UI
+{
+    /// <summary>
+    /// An independent portion of a screen and its contents.
+    /// </summary>
+    public sealed class Window : MonoBehaviour
+    {
+        [Header("Initialization")]
+        public EventBus communicatesWith = null;
+
+        private void Start()
+        {
+            if (!communicatesWith)
+            {
+                throw new InvalidOperationException(
+                    "communicatesWith must not be null");
+            }
+        }
+    }
+}
---
+++ Assets/StartGame/UI/Tests/Runtime/WindowSpec.cs
@@ -0,0 +1,28 @@
+﻿using Sakura.StartGame.Domain;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Sakura.StartGame.UI.Tests
+{
+    public sealed class WindowSpec
+    {
+        [UnityTest]
+        public IEnumerator Windows_created_without_an_event_bus_are_invalid()
+        {
+            var simulation = ScriptableObject.CreateInstance<Simulation>();
+            var eventBusGameObject = new GameObject("Event Bus");
+            var eventBus = eventBusGameObject.AddComponent<EventBus>();
+            eventBus.forSimulation = simulation;
+            var windowGameObject = new GameObject("Window");
+            var window = windowGameObject.AddComponent<Window>();
+            yield return null;
+            LogAssert.Expect(
+                LogType.Exception,
+                "InvalidOperationException: communicatesWith must not be null");
+            Object.Destroy(simulation);
+            Object.Destroy(eventBusGameObject);
+            Object.Destroy(windowGameObject);
+        }
+    }
+}
