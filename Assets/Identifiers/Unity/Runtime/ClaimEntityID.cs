745f92c81d2976b33268b8abfa4e9308192a93ed
Wesley Brown <wesleybrown@wesleybrown.net>
2021-04-02 20:23:36 -0400

    Create claim entity ID component
    Create the component that will allow a game object to claim a given entity ID
    for itself.

---
+++ Assets/Identifiers/Unity/Runtime/ClaimEntityID.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Sakura.Identifiers.Client;
+using UnityEngine;
+
+namespace Sakura.Identifiers.Unity
+{
+    /// <summary>
+    ///     Claim an entity ID for an attached game object.
+    /// </summary>
+    public sealed class ClaimEntityID : MonoBehaviour
+    {
+        /// <summary>
+        ///     The entity ID to claim.
+        /// </summary>
+        [Tooltip("The entity ID to claim.")]
+        public string entityID = "";
+
+        /// <summary>
+        ///     The ID registry to make the claim with.
+        /// </summary>
+        [Tooltip("The ID registry to make the claim with.")]
+        public ScriptableObjectIdRegistry registry = null;
+
+        private void Start()
+        {
+            Debug.Assert(entityID != "", "entityID must not be empty.");
+            Debug.Assert(registry != null, "registry must not be null.");
+            var claimsAuthority = new ClaimsAgent(registry);
+            var ID = new Guid(entityID);
+            if (!claimsAuthority.IsClaimed(ID))
+                claimsAuthority.ClaimIDFor(ID, gameObject.GetInstanceID());
+            else
+                Debug.LogError("Entity ID '" + ID + "' is already claimed.");
+        }
+    }
+}
---
+++ Assets/Identifiers/Unity/Runtime/ScriptableObjectIdRegistry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Sakura.Identifiers.Client;
+using Sakura.Identifiers.Infrastructure;
+using UnityEngine;
+
+namespace Sakura.Identifiers.Unity
+{
+    /// <summary>
+    ///     An ID registry that is also a scriptable object.
+    /// </summary>
+    [CreateAssetMenu(
+        fileName = "NewScriptableObjectIdRegistry",
+        menuName = "Identifiers/ID Registries/ScriptableObjectIdRegistry")]
+    public sealed class ScriptableObjectIdRegistry
+        : ScriptableObject, IdRegistry
+    {
+        private DictionaryIdRegistry registry;
+
+        private void OnEnable()
+        {
+            registry = new DictionaryIdRegistry();
+        }
+
+        public void ClaimIdFor(Guid ID, int instanceID)
+        {
+            registry.ClaimIdFor(ID, instanceID);
+        }
+
+        public GameObject GameObjectClaimingId(Guid ID)
+        {
+            return registry.GameObjectClaimingId(ID);
+        }
+    }
+}
