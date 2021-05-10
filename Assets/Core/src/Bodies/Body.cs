ebb2f5139ff51fc4db242cbb3f9442674354abfe
Wesley Brown <wesleybrown@wesleybrown.net>
2021-05-09 20:19:06 -0400

    Create body
    A body that is created with the nil UUID should throw an exception.

---
+++ Assets/Core/src/Bodies/Body.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sakura.Core.Bodies
+{
+    /// <summary>
+    ///     A physical representation of an entity.
+    /// </summary>
+    public sealed class Body
+    {
+        /// <summary>
+        ///     Create a new body with a given entity ID.
+        /// </summary>
+        /// <param name="entityID">
+        ///     The entity ID.
+        /// </param>
+        public Body(Guid entityID)
+        {
+            if (entityID == Guid.Empty)
+                throw new ArgumentException(
+                    "A body must not be created with the nil UUID",
+                    nameof(entityID));
+        }
+    }
+}
---
+++ Assets/Core/tests/Bodies/BodyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sakura.Core.Bodies.Tests
+{
+    [TestFixture]
+    public class A_body
+    {
+        [Test]
+        public void Throws_when_created_with_the_nil_UUID()
+        {
+            var ID = Guid.Empty;
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Body(ID);
+            });
+        }
+    }
+}
