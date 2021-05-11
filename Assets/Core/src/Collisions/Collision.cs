2101b5ad6ff7fc5c2cff422bd06703038ec2649a
Wesley Brown <wesleybrown@wesleybrown.net>
2021-05-11 18:50:15 -0400

    Create collision
    A collision must be created with a colliding body.

---
+++ Assets/Core/src/Collisions/Collision.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sakura.Core
+{
+    /// <summary>
+    ///     A collision between a colliding body and another body.
+    /// </summary>
+    public sealed class Collision
+    {
+        /// <summary>
+        ///     Create a new collision between a given colliding body and
+        ///     another given body.
+        /// </summary>
+        /// <param name="collider">
+        ///     The colliding body.
+        /// </param>
+        /// <param name="other">
+        ///     The other body.
+        /// </param>
+        /// <returns>
+        ///     A newly created collision between the given colliding body and
+        ///     the other given body.
+        /// </returns>
+        public static Collision ForBodyCollidingWithOther(
+            Body collider,
+            Body other)
+        {
+            if (collider == null)
+                throw new ArgumentNullException(
+                    "The given collider must not be null",
+                    nameof(collider));
+            return null;
+        }
+    }
+}
---
+++ Assets/Core/tests/Collisions/CollisionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Sakura.Core.Tests
+{
+    [TestFixture]
+    public class A_collision
+    {
+        [Test]
+        public void Throws_when_created_without_a_colliding_body()
+        {
+            var ID = new Guid("b45ef316-b840-4bd6-9789-3f0a9ecf679c");
+            var location = new Vector3(10.0f, 10.0f, 10.0f);
+            var otherBody = new Body(ID, location);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                Collision.ForBodyCollidingWithOther(
+                    null,
+                    otherBody);
+            });
+        }
+    }
+}
