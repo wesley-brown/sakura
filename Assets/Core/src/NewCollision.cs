8120a60446a7423963c9a7875dede0770383d9c3
Wesley Brown <wesleybrown@wesleybrown.net>
2021-05-17 16:22:56 -0400

    Create new collision
    The new collision type will be created. It will eventually replace the current
    collision type.

---
+++ Assets/Core/src/NewCollision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sakura.Core
+{
+    public sealed class NewCollision
+    {
+        public NewCollision(
+            Movement movement,
+            Body body)
+        {
+            if (movement == null)
+                throw new ArgumentNullException(
+                    nameof(movement),
+                    "The given movement must not be null");
+        }
+    }
+}
---
+++ Assets/Core/tests/NewCollisionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Core;
+using UnityEngine;
+
+namespace New_collision_spec
+{
+    [TestFixture]
+    public class A_new_collision
+    {
+        [Test]
+        public void Throws_when_created_without_a_movement()
+        {
+            var ID = new Guid("b45ef316-b840-4bd6-9789-3f0a9ecf679c");
+            var location = new Vector3(10.0f, 10.0f, 10.0f);
+            var body = new Body(ID, location);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new NewCollision(null, body);
+            });
+        }
+    }
+}
