0d6a8d387ee60065b184c815857a52a839782c37
Wesley Brown <wesleybrown@wesleybrown.net>
2021-05-17 14:15:52 -0400

    Create movement
    Identical movements have the same hash codes.

---
+++ Assets/Core/src/Movement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Sakura.Core
+{
+    /// <summary>
+    ///     An act of moving a body to a location.
+    /// </summary>
+    public sealed class Movement
+    {
+        /// <summary>
+        ///     Create a new movement for moving a given body to a given
+        ///     location.
+        /// </summary>
+        /// <param name="body">
+        ///     The body to move.
+        /// </param>
+        /// <param name="location">
+        ///     The location to move the body to.
+        /// </param>
+        /// <returns>
+        ///     A new movement that represents the act of moving the given body
+        ///     to the given location.
+        /// </returns>
+        public static Movement For(
+            Body body,
+            Vector3 location)
+        {
+            return new Movement(body, location);
+        }
+
+        private readonly Body body;
+        private readonly Vector3 location;
+
+        private Movement(
+            Body body,
+            Vector3 location)
+        {
+            this.body = body;
+            this.location = location;
+        }
+
+        public Body Body()
+        {
+            return body;
+        }
+
+        public Vector3 Location()
+        {
+            return location;
+        }
+
+        public override int GetHashCode()
+        {
+            // Joshua Bloch's hash code recipe from Effective Java, 3rd
+            // Edition, page 53.
+            var hashCode = body.GetHashCode();
+            hashCode = 31 * hashCode + location.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ Assets/Core/tests/MovementTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Sakura.Core
+{
+    [TestFixture]
+    public class Identical_movements
+    {
+        [Test]
+        public void Have_the_same_hash_codes()
+        {
+            var ID = new Guid("e72a1ee5-f845-44e1-a196-1cf7999e6a66");
+            var location = new Vector3(2.0f, 0.0f, 2.0f);
+            var body = new Body(ID, location);
+            var destination = new Vector3(3.0f, 0.0f, 2.0f);
+            var movement = Movement.For(body, destination);
+            var duplicateMovement = Movement.For(body, destination);
+            Assert.AreEqual(
+                movement.GetHashCode(),
+                duplicateMovement.GetHashCode());
+        }
+    }
+}
