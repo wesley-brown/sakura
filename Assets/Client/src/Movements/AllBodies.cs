8be7829753d15264436128e20a51116bf1f31c33
Wesley Brown <wesleybrown@wesleybrown.net>
2021-05-25 21:15:35 -0400

    Create dictionary body collection
    A body added to a dictionary body collection will be able to be retrieved.

---
+++ Assets/Client/src/Movements/AllBodies.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Sakura.Movements
+{
+    /// <summary>
+    ///     A collection of all bodies in the simulation.
+    /// </summary>
+    public interface AllBodies
+    {
+        /// <summary>
+        ///     Whether or not there is a body for an entity with a given ID
+        ///     in the simulation.
+        /// </summary>
+        /// <param name="entityID">
+        ///     The ID of the entity.
+        /// </param>
+        /// <returns>
+        ///     True if there is a body for the entity with the given ID in the
+        ///     simulation; false otherwise.
+        /// </returns>
+        bool HasBodyFor(Guid entityID);
+
+        /// <summary>
+        ///     Add a given body to the collection of all bodies.
+        /// </summary>
+        /// <param name="body">
+        ///     The body to add.
+        /// </param>
+        /// <returns>
+        ///     A new colleciton of all bodies containing all the bodies this
+        ///     current one has plus the given body to add.
+        /// </returns>
+        AllBodies Add(Body body);
+
+        /// <summary>
+        ///     The body for the entity with a given ID.
+        /// </summary>
+        /// <param name="entityID">
+        ///     The ID of the entity.
+        /// </param>
+        /// <returns>
+        ///     The body for the entity with the given ID.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown when the entity with the given ID has no body.
+        /// </exception>
+        Body For(Guid entityID);
+    }
+}
---
+++ Assets/Infrastructure/tests/Movements/DictionaryBodyCollectionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Movements;
+using UnityEngine;
+
+namespace Dictionary_body_collection_spec
+{
+    [TestFixture]
+    public class An_added_body
+    {
+        [Test]
+        public void Can_be_retrieved()
+        {
+            var ID = new Guid("da68712c-cdcc-4bf8-8984-98032ef3aa4d");
+            var location = new Vector3(1.0f, 1.0f, 1.0f);
+            var body = new Body(
+                ID,
+                location);
+            AllBodies bodies = DictionaryBodyCollection.Empty();
+            Assert.IsFalse(
+                bodies.HasBodyFor(
+                    body.EntityID()));
+            bodies = bodies.Add(body);
+            Assert.IsTrue(
+                bodies.HasBodyFor(
+                    body.EntityID()));
+            var retrievedBody = bodies.For(body.EntityID());
+            Assert.AreEqual(body, retrievedBody);
+        }
+    }
+}
