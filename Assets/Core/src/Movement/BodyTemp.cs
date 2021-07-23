ed2306a85c3ebd56b8e94af69970ecf13e9a0f1e
Wesley Brown <wesleybrown@wesleybrown.net>
2021-07-23 19:22:32 -0400

    Create body replacement
    A cannot be created for the nil entity.

---
+++ Assets/Core/src/Movement/BodyTemp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sakura.Core
+{
+    /// <summary>
+    ///     A physical representation of an entity.
+    /// </summary>
+    public sealed class BodyTemp
+    {
+        /// <summary>
+        ///     Create a body for an entity at a given location.
+        /// </summary>
+        /// <param name="entity">
+        ///     The entity.
+        /// </param>
+        /// <param name="location">
+        ///     The location.
+        /// </param>
+        /// <returns>
+        ///     A body for the given entity at the given location.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown when the given entity is the nil entity.
+        /// </exception>
+        public static BodyTemp ForEntityLocatedAt(
+            Guid entity,
+            Vector3 location)
+        {
+            if (entity == Guid.Empty)
+                throw new ArgumentException(
+                    "The nil entity cannot have a body.",
+                    nameof(entity));
+            return null;
+        }
+    }
+}
---
+++ Assets/Core/tests/Movement/BodyTempSpec.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Core;
+using UnityEngine;
+
+namespace Body_Temp_Spec
+{
+    [TestFixture]
+    public class A_body
+    {
+        [Test]
+        public void Cannot_be_created_for_the_nil_entity()
+        {
+            var entity = Guid.Empty;
+            var location = Vector3.zero;
+            Assert.Throws<ArgumentException>(() =>
+            {
+                BodyTemp.ForEntityLocatedAt(
+                    entity,
+                    location);
+            });
+        }
+    }
+}
