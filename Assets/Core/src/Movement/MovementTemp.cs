690d069dea23bda1b3ac99b4c4d6129836baac4b
Wesley Brown <wesleybrown@wesleybrown.net>
2021-08-27 14:55:34 -0400

    Create movement replacement
    Creating a movement for a null body will not be supported.

---
+++ Assets/Core/src/Movement/MovementTemp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sakura.Core
+{
+    /// <summary>
+    ///     A displacement of a body through world space.
+    /// </summary>
+    public sealed class MovementTemp
+    {
+        /// <summary>
+        ///     Create a movement for a given body.
+        /// </summary>
+        /// <param name="body">
+        ///     The body.
+        /// </param>
+        /// <returns>
+        ///     A movement for the given body.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the given body is null.
+        /// </exception>
+        public static MovementTemp For(BodyTemp body)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(
+                    nameof(body),
+                    "The given body must not be null.");
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Core/tests/Movement/MovementTempSpec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Core;
+using UnityEngine;
+
+namespace Movement_Temp_Spec
+{
+    [TestFixture]
+    public class Creating_a_movement_for_a_body
+    {
+        [Test]
+        public void Does_not_support_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                MovementTemp.For(null);
+            });
+        }
+    }
+}
