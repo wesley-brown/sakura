9d8d82cc9b183b06bf52582233e4d3213e806cfc
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-11 13:36:26 -0400

    Create collision replacement
    Creating a collision with an adjusted movement towards a body will not
    support a null adjusted movement.

---
+++ Assets/Core/src/Movement/CollisionTemp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sakura.Core
+{
+    /// <summary>
+    ///     An instance of one body trying to occupy the same space as another
+    ///     after moving.
+    /// </summary>
+    public sealed class CollisionTemp
+    {
+        /// <summary>
+        ///     Create a collision with a given adjustd movement towards a 
+        ///     given body.
+        /// </summary>
+        /// <param name="adjustedMovement">
+        ///     The adjusted movement.
+        /// </param>
+        /// <param name="body">
+        ///     The body the adjusted movement is towards.
+        /// </param>
+        /// <returns>
+        ///     A collision with the given adjusted movement towards the given
+        ///     body.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the given adjusted movement is null.
+        /// </exception>
+        public static CollisionTemp WithAdjustedMovementTowardsBody(
+            MovementTemp adjustedMovement,
+            BodyTemp body)
+        {
+            if (adjustedMovement == null)
+                throw new ArgumentNullException(nameof(adjustedMovement));
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Core/tests/Movement/CollisionTempSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Core;
+using UnityEngine;
+
+namespace Collision_Temp_Spec
+{
+    [TestFixture]
+    public class Creating_a_collision_with_an_adjusted_movement_towards_a_body
+    {
+        [Test]
+        public void Does_not_support_a_null_movement()
+        {
+            MovementTemp movement = null;
+            var entity = new Guid("0a35997f-1c46-426d-a9ee-dcbca907cae9");
+            var location = new Vector3(5.0f, 0.0f, 0.0f);
+            var body = BodyTemp.ForEntityLocatedAt(
+                entity,
+                location);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                CollisionTemp.WithAdjustedMovementTowardsBody(
+                    movement,
+                    body);
+            });
+        }
+    }
+}
