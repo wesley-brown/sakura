f04b5b1516cc6da5da6ee932d00871baccebddde
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-11 15:19:45 -0400

    Create collision reaction
    Creating a collision reaction for two collided bodies will not support a null
    collider body.

---
+++ Assets/Core/src/Movement/CollisionReaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sakura.Core
+{
+    /// <summary>
+    ///     A instance of two bodies reacting to a collision between them.
+    /// </summary>
+    public sealed class CollisionReaction
+    {
+        /// <summary>
+        ///     Create a <see cref="CollisionReaction"/> for two given collided
+        ///     bodies.
+        /// </summary>
+        /// <param name="collider">
+        ///     The body that made the collision.
+        /// </param>
+        /// <param name="collidee">
+        ///     The body that was collided with.
+        /// </param>
+        /// <returns>
+        ///     A <see cref="CollisionReaction"/> for the two given collided
+        ///     bodies.
+        /// </returns>
+        public static CollisionReaction ForCollidedBodies(
+            BodyTemp collider,
+            BodyTemp collidee)
+        {
+            if (collider == null)
+                throw new ArgumentNullException(nameof(collider));
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Core/tests/Movement/CollisionReactionSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Core;
+using UnityEngine;
+
+namespace Collision_Reaction_Spec
+{
+    [TestFixture]
+    public class Creating_a_collision_reaction_for_two_collided_bodies
+    {
+        [Test]
+        public void Does_not_support_a_null_collider_body()
+        {
+            BodyTemp collidersBody = null;
+            var collidee = new Guid("8910b086-e969-48f4-b915-89bc8fe4432f");
+            var collideesLocation = new Vector3(5.0f, 0.0f, 0.0f);
+            var collideesBody = BodyTemp.ForEntityLocatedAt(
+                collidee,
+                collideesLocation);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                CollisionReaction.ForCollidedBodies(
+                    collidersBody,
+                    collideesBody);
+            });
+        }
+    }
+}
