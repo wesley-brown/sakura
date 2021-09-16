7e93dec12af7a9ef268f2109b48bf3ea5cafcb22
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-16 10:46:49 -0400

    Rewrite collision
    Remove old implementation of collision.

--- Assets/Core/src/Movement/Collision.cs
+++ Assets/Core/src/Movement/Collision.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace Sakura.Core
 {
@@ -10,47 +9,11 @@ namespace Sakura.Core
     public sealed class Collision
     {
         /// <summary>
-        ///     Create a <see cref="Collision"/> with a given adjusted
-        ///     <see cref="Movement"/> towards a given body.
-        /// </summary>
-        /// <param name="adjustedMovement">
-        ///     The adjusted <see cref="Movement"/>.
-        /// </param>
-        /// <param name="body">
-        ///     The <see cref="Body"/> the adjusted <see cref="Movement"/> is
-        ///     towards.
-        /// </param>
-        /// <returns>
-        ///     A <see cref="Collision"/> with the given adjusted
-        ///     <see cref="Movement"/> towards the given <see cref="Body"/>.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Thrown when the given adjusted <see cref="Movement"/> is null.
-        ///
-        ///     -or-
-        ///
-        ///     Thrown when the given target <see cref="Body"/> is null.
-        /// </exception>
-        public static Collision WithAdjustedMovementTowardsTargetBody(
-            Movement adjustedMovement,
-            Body target)
-        {
-            if (adjustedMovement == null)
-                throw new ArgumentNullException(nameof(adjustedMovement));
-            if (target == null)
-                throw new ArgumentNullException(nameof(target));
-            return new Collision(
-                new Guid(),
-                adjustedMovement,
-                target);
-        }
-
-        /// <summary>
         ///     Create a <see cref="Collision"/> between two given
         ///     <see cref="Body"/>s.
         /// </summary>
         /// <param name="collider">
-        ///     The <see cref="Body"/> that caused the collision by moving.
+        ///     The <see cref="Body"/> that moved.
         /// </param>
         /// <param name="collidee">
         ///     The <see cref="Body"/> that was moved into.
@@ -75,76 +38,6 @@ namespace Sakura.Core
             if (collidee == null)
                 throw new ArgumentNullException(nameof(collidee));
             throw new NotImplementedException();
-        }
-
-        private Collision(
-            Guid id,
-            Movement adjustedMovement,
-            Body target)
-        {
-            this.id = id;
-            Debug.Assert(adjustedMovement != null);
-            this.adjustedMovement = adjustedMovement;
-            Debug.Assert(target != null);
-            this.target = target;
-        }
-
-        private readonly Guid id;
-        private readonly Movement adjustedMovement;
-        private readonly Body target;
-
-        /// <summary>
-        ///     The ID of this <see cref="Collision"/>.
-        /// </summary>
-        public Guid ID
-        {
-            get
-            {
-                return id;
-            }
-        }
-
-        /// <summary>
-        ///     The adjusted <see cref="Movement"/> for the colliding body
-        ///     after collisions are taken into account.
-        /// </summary>
-        public Movement AdjustedMovement
-        {
-            get
-            {
-                return adjustedMovement;
-            }
-        }
-
-        /// <summary>
-        ///     The target <see cref="Body"/> the adjusted
-        ///     <see cref="Movement"/> is towards.
-        /// </summary>
-        public Body Target
-        {
-            get
-            {
-                return target;
-            }
-        }
-
-        /// <summary>
-        ///     Create a string representation of this
-        ///     <see cref="Collision"/>.
-        /// </summary>
-        /// <returns>
-        ///     A string representation of this <see cref="Collision"/>.
-        /// </returns>
-        public override string ToString()
-        {
-            return "{"
-                + "ID="
-                + ID
-                + ", AdjustedMovement="
-                + AdjustedMovement
-                + ", Target="
-                + Target
-                + "}";
         }
     }
 }
--- Assets/Core/tests/Movement/CollisionSpec.cs
+++ Assets/Core/tests/Movement/CollisionSpec.cs
@@ -5,49 +5,6 @@ using Sakura.Core;
 namespace Collision_Spec
 {
     [TestFixture]
-    public class Creating_a_collision_with_an_adjusted_movement_towards_a_target_body
-    {
-        [Test]
-        public void Does_not_support_a_null_movement()
-        {
-            Movement movement = null;
-            var entity = new Guid("0a35997f-1c46-426d-a9ee-dcbca907cae9");
-            var location = new UnityEngine.Vector3(5.0f, 0.0f, 0.0f);
-            var body = Body.ForEntityLocatedAt(
-                entity,
-                location);
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                Collision.WithAdjustedMovementTowardsTargetBody(
-                    movement,
-                    body);
-            });
-        }
-
-        [Test]
-        public void Does_not_support_a_null_body()
-        {
-            var player = new Guid("db8956a2-2b44-40a8-ab9e-8e0e4f064220");
-            var playersLocation = new UnityEngine.Vector3(3.0f, 0.0f, 0.0f);
-            var playersBody = Body.ForEntityLocatedAt(
-                player,
-                playersLocation);
-            var playersAdjustedDestination =
-                new UnityEngine.Vector3(4.0f, 0.0f, 0.0f);
-            var playersAdjustedMovement = Movement.For(
-                playersBody,
-                playersAdjustedDestination);
-            Body enemyBody = null;
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                Collision.WithAdjustedMovementTowardsTargetBody(
-                    playersAdjustedMovement,
-                    enemyBody);
-            });
-        }
-    }
-
-    [TestFixture]
     public class Creating_a_collision_between_two_bodies
     {
         [Test]
@@ -82,59 +39,6 @@ namespace Collision_Spec
                     colliderBody,
                     collideeBody);
             });
-        }
-    }
-
-    [TestFixture]
-    public class The_string_representation_of_a_collision
-    {
-        [Test]
-        public void Contains_its_ID()
-        {
-            var collision = CreateCollision();
-            StringAssert.Contains(
-                collision.ID.ToString(),
-                collision.ToString());
-        }
-
-        private static Collision CreateCollision()
-        {
-            var player = new Guid("db8956a2-2b44-40a8-ab9e-8e0e4f064220");
-            var playersLocation = new UnityEngine.Vector3(3.0f, 0.0f, 0.0f);
-            var playersBody = Body.ForEntityLocatedAt(
-                player,
-                playersLocation);
-            var playersAdjustedDestination =
-                new UnityEngine.Vector3(4.0f, 0.0f, 0.0f);
-            var playersAdjustedMovement = Movement.For(
-                playersBody,
-                playersAdjustedDestination);
-            var enemy = new Guid("0a35997f-1c46-426d-a9ee-dcbca907cae9");
-            var enemysLocation = new UnityEngine.Vector3(5.0f, 0.0f, 0.0f);
-            var enemysBody = Body.ForEntityLocatedAt(
-                enemy,
-                enemysLocation);
-            return Collision.WithAdjustedMovementTowardsTargetBody(
-                playersAdjustedMovement,
-                enemysBody);
-        }
-
-        [Test]
-        public void Contains_its_adjusted_movement()
-        {
-            var collision = CreateCollision();
-            StringAssert.Contains(
-                collision.AdjustedMovement.ToString(),
-                collision.ToString());
-        }
-
-        [Test]
-        public void Contains_its_target_body()
-        {
-            var collision = CreateCollision();
-            StringAssert.Contains(
-                collision.Target.ToString(),
-                collision.ToString());
         }
     }
 }
