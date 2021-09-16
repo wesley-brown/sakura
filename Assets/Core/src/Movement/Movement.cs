3f4f82b03fddce277246fd1567b5c1b18d8f5d34
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-16 11:31:36 -0400

    Rewrite movement
    The Movement type will be rewritten and anything relying on it will be
    removed.

--- Assets/Core/src/Movement/Movement.cs
+++ Assets/Core/src/Movement/Movement.cs
@@ -4,41 +4,11 @@ using UnityEngine;
 namespace Sakura.Core
 {
     /// <summary>
-    ///     A displacement of a <see cref="Core.Body"/> through world space to a
+    ///     A displacement of a <see cref="Body"/> through world space to a
     ///     destination.
     /// </summary>
     public sealed class Movement
     {
-        /// <summary>
-        ///     Create a <see cref="Movement"/> for a given
-        ///     <see cref="Core.Body"/> and destination.
-        /// </summary>
-        /// <param name="body">
-        ///     The <see cref="Core.Body"/> being moved.
-        /// </param>
-        /// <param name="destination">
-        ///     The destination.
-        /// </param>
-        /// <returns>
-        ///     A <see cref="Movement"/> for the given <see cref="Core.Body"/>
-        ///     and destination.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Thrown when the given <see cref="Core.Body"/> is null.
-        /// </exception>
-        public static Movement For(
-            Body body,
-            Vector3 destination)
-        {
-            if (body == null)
-                throw new ArgumentNullException(
-                    nameof(body),
-                    "The given body must not be null.");
-            return new Movement(
-                body,
-                destination);
-        }
-
         /// <summary>
         ///     Create a <see cref="Movement"/> towards a given destination
         ///     with a given speed.
@@ -65,61 +35,6 @@ namespace Sakura.Core
                     nameof(speed),
                     "The given speed must be non-negative.");
             throw new NotImplementedException();
-        }
-
-        private Movement(
-            Body body,
-            Vector3 destination)
-        {
-            Debug.Assert(body != null);
-            this.body = body;
-            this.destination = new Vector3(
-                destination.x,
-                destination.y,
-                destination.z);
-        }
-
-        private readonly Body body;
-        private readonly Vector3 destination;
-
-        /// <summary>
-        ///     The <see cref="Core.Body"/> to move.
-        /// </summary>
-        public Body Body
-        {
-            get
-            {
-                return body;
-            }
-        }
-
-        /// <summary>
-        ///     Create a <see cref="Core.Body"/> that is the result of moving
-        ///     the <see cref="Core.Body"/> to the destination.
-        /// </summary>
-        /// <returns>
-        ///     The <see cref="Core.Body"/> that is the result of moving the
-        ///     <see cref="Core.Body"/> to the destination.
-        /// </returns>
-        public Body ResultingBody()
-        {
-            return body.TeleportTo(destination);
-        }
-
-        /// <summary>
-        ///     Create a string representation of this <see cref="Movement"/>.
-        /// </summary>
-        /// <returns>
-        ///     A string representation of this <see cref="Movement"/>.
-        /// </returns>
-        public override string ToString()
-        {
-            return "{"
-                + "Body="
-                + Body
-                + ", Resulting Body="
-                + ResultingBody()
-                + "}";
         }
     }
 }
--- Assets/Core/tests/Movement/MovementSpec.cs
+++ Assets/Core/tests/Movement/MovementSpec.cs
@@ -6,23 +6,6 @@ using UnityEngine;
 namespace Movement_Spec
 {
     [TestFixture]
-    public class Creating_a_movement_for_a_body_and_destination
-    {
-        [Test]
-        public void Does_not_support_a_null_body()
-        {
-            Body body = null;
-            var destination = new Vector3(10.0f, 10.0f, 10.0f);
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                Movement.For(
-                    body,
-                    destination);
-            });
-        }
-    }
-
-    [TestFixture]
     public class Creating_a_movement_towards_a_destination_with_a_speed
     {
         [TestCase(-0.99f)]
@@ -37,41 +20,6 @@ namespace Movement_Spec
                     destination,
                     speed);
             });
-        }
-    }
-
-    [TestFixture]
-    public class The_string_representation_of_a_movement
-    {
-        [Test]
-        public void Includes_its_body()
-        {
-            var movement = Movement();
-            StringAssert.Contains(
-                movement.Body.ToString(),
-                movement.ToString());
-        }
-
-        [Test]
-        public void Includes_its_resulting_body()
-        {
-            var movement = Movement();
-            StringAssert.Contains(
-                movement.ResultingBody().ToString(),
-                movement.ToString());
-        }
-
-        private static Movement Movement()
-        {
-            var ID = new Guid("12c90c8a-9d7a-4a58-a9b3-f457496c9fba");
-            var startingLocation = new Vector3(-5.0f, 0.0f, 0.0f);
-            var body = Body.ForEntityLocatedAt(
-                ID,
-                startingLocation);
-            var destination = new Vector3(0.0f, 0.0f, 0.0f);
-            return Sakura.Core.Movement.For(
-                body,
-                destination);
         }
     }
 }
