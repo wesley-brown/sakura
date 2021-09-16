060714f254657b598e8f725453508d29a5de575c
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-15 23:02:18 -0400

    Update movement
    Creating a movement towards a destination with a speed will not support a
    negative speed.

--- Assets/Core/src/Movement/Movement.cs
+++ Assets/Core/src/Movement/Movement.cs
@@ -39,6 +39,34 @@ namespace Sakura.Core
                 destination);
         }
 
+        /// <summary>
+        ///     Create a <see cref="Movement"/> towards a given destination
+        ///     with a given speed.
+        /// </summary>
+        /// <param name="destination">
+        ///     The destination to move towards.
+        /// </param>
+        /// <param name="speed">
+        ///     The speed to move towards the destination with.
+        /// </param>
+        /// <returns>
+        ///     A <see cref="Movement"/> that moves towards the given
+        ///     destination with the given speed.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when the given speed is negative.
+        /// </exception>
+        public static Movement TowardsDestinationWithSpeed(
+            Vector3 destination,
+            float speed)
+        {
+            if (speed < 0)
+                throw new ArgumentOutOfRangeException(
+                    nameof(speed),
+                    "The given speed must be non-negative.");
+            throw new NotImplementedException();
+        }
+
         private Movement(
             Body body,
             Vector3 destination)
--- Assets/Core/tests/Movement/MovementSpec.cs
+++ Assets/Core/tests/Movement/MovementSpec.cs
@@ -23,6 +23,24 @@ namespace Movement_Spec
     }
 
     [TestFixture]
+    public class Creating_a_movement_towards_a_destination_with_a_speed
+    {
+        [TestCase(-0.99f)]
+        [TestCase(-1.0f)]
+        [TestCase(float.MinValue)]
+        public void Does_not_support_a_negative_speed(float speed)
+        {
+            var destination = new Vector3(2.0f, 0.0f, 0.0f);
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                Movement.TowardsDestinationWithSpeed(
+                    destination,
+                    speed);
+            });
+        }
+    }
+
+    [TestFixture]
     public class The_string_representation_of_a_movement
     {
         [Test]
