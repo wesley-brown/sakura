832e468e006beb1a1ca77b71587dc7a2ecb42d00
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-16 20:27:47 -0400

    Update movement spec
    A missing test that documents how moving a body towards a destination
    exactly fast enough will move it to the destination will be added.

--- Assets/Core/tests/Movement/MovementSpec.cs
+++ Assets/Core/tests/Movement/MovementSpec.cs
@@ -42,7 +42,7 @@ namespace Movement_Spec
                 speed);
             var movedBody = movement.Move(body);
             Assert.AreEqual(
-                body.Entity,
+                entity,
                 movedBody.Entity);
             Assert.AreEqual(
                 expectedLocation,
@@ -91,6 +91,72 @@ namespace Movement_Spec
                     -5 + (float)(Math.Sqrt(3) / 3),
                     -5 + (float)(Math.Sqrt(3) / 3),
                     -5 + (float)(Math.Sqrt(3) / 3))     // ending location
+            }
+        };
+    }
+
+    [TestFixture]
+    public class Moving_a_body_towards_a_destination_exactly_fast_enough
+    {
+        [TestCaseSource(nameof(Cases))]
+        public void Moves_it_to_the_destination_in_one_tick(
+            Vector3 startingLocation,
+            Vector3 destination,
+            float speed,
+            Vector3 expectedLocation)
+        {
+            var entity = new Guid("862fb4eb-0154-4ad7-913b-916341af4f12");
+            var body = Body.ForEntityLocatedAt(
+                entity,
+                startingLocation);
+            var movement = Movement.TowardsDestinationWithSpeed(
+                destination,
+                speed);
+            var movedBody = movement.Move(body);
+            Assert.AreEqual(
+                movedBody.Entity,
+                entity);
+            Assert.AreEqual(
+                expectedLocation,
+                movedBody.Location);
+        }
+
+        static readonly object[] Cases =
+        {
+            // x-axis aligned movement
+            new object[]
+            {
+                new Vector3(0.0f, 0.0f, 0.0f),  // starting location
+                new Vector3(1.0f, 0.0f, 0.0f),  // destination
+                1.0f,                           // speed
+                new Vector3(1.0f, 0.0f, 0.0f)   // ending location
+            },
+
+            // z-axis aligned movement
+            new object[]
+            {
+                new Vector3(0.0f, 0.0f, -1.0f), // starting location
+                new Vector3(0.0f, 0.0f, 1.0f),  // destination
+                2.0f,                           // speed
+                new Vector3(0.0f, 0.0f, 1.0f)   // ending location
+            },
+
+            // x-axis and z-axis aligned movement
+            new object[]
+            {
+                new Vector3(-10.0f, 0.0f, 10.0f),   // starting location
+                new Vector3(10.0f, 0.0f, -10.0f),   // destination
+                20 * (float)Math.Sqrt(2),           // speed
+                new Vector3(10.0f, 0.0f, -10.0f)    // ending location
+            },
+
+            // x-axis, y-axis, and z-axis movement
+            new object[]
+            {
+                new Vector3(-1.5f, -1.5f, -1.5f),   // starting location
+                new Vector3(-4.5f, -4.5f, -4.5f),   // destination
+                3 * (float)Math.Sqrt(3),            // speed
+                new Vector3(-4.5f, -4.5f, -4.5f)    // ending location
             }
         };
     }
