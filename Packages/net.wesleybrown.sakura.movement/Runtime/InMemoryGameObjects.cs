ed4f09673ba90830a232b940b5f9cacc0b1dd896
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-27 13:00:16 -0400

    Create in-memory game objects
    An empty collection of in-memory game objects will not have any game objects.

---
+++ Packages/net.wesleybrown.sakura.movement/Runtime/InMemoryGameObjects.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sakura.Data
+{
+    /// <summary>
+    ///     A <see cref="GameObjects"/> that stores all
+    ///     <see cref="GameObjects"/> in memory.  
+    /// </summary>
+    sealed class InMemoryGameObjects : GameObjects
+    {
+        /// <summary>
+        ///     Create a <see cref="InMemoryGameObjects"/> that doesn't have
+        ///     any <see cref="GameObject"/>s in it.
+        /// </summary>
+        /// <returns>
+        ///     An empty <see cref="InMemoryGameObjects"/>.
+        /// </returns>
+        internal static InMemoryGameObjects Empty()
+        {
+            return new InMemoryGameObjects();
+        }
+
+        private InMemoryGameObjects()
+        {
+            gameObjects = new Dictionary<Guid, GameObject>();
+        }
+
+        private readonly Dictionary<Guid, GameObject> gameObjects;
+
+        /// <inheritdoc/>
+        public void AddGameObjectForEntity(
+            GameObject gameObject,
+            Guid entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public GameObject GameObjectForEntity(Guid entity)
+        {
+            if (!gameObjects.ContainsKey(entity))
+                return null;
+            return gameObjects[entity];
+        }
+
+        /// <inheritdoc/>
+        public Guid EntityForGameObject(GameObject gameObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Packages/net.wesleybrown.sakura.movement/Tests/Runtime/InMemoryGameObjectsSpec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Sakura.Data;
+
+namespace In_Memory_Game_Objects_Spec
+{
+    [TestFixture]
+    public class An_empty_collection_of_in_memory_game_objects
+    {
+        [Test]
+        public void Has_no_game_objects()
+        {
+            var gameObjects = InMemoryGameObjects.Empty();
+            var entity = new Guid("39de7404-4ac8-427a-9943-7a215ddc470b");
+            Assert.IsNull(gameObjects.GameObjectForEntity(entity));
+        }
+    }
+}
