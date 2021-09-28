68551149b26e03f6a6b1ee2d3c3c5b2346f5e485
Wesley Brown <wesleybrown@wesleybrown.net>
2021-09-28 15:53:58 -0400

    Rename movement package
    The Sakura Movement package will be renamed to Sakura Bodies.

---
+++ Packages/net.wesleybrown.sakura.bodies/Runtime/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Sakura.Bodies.Tests")]
--- Packages/net.wesleybrown.sakura.movement/Runtime/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Sakura.Movement.Tests")]
