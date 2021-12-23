3f6a0992cf67c6e7f47005387b64539910625e41
Wesley Brown <wesleybrown@wesleybrown.net>
2021-12-23 16:36:47 -0500

    Update record collidee
    Record collidees will be updated to retrieve the root game object of game
    objects they collide with so that colliding with nested colliders on game
    game objects retrieves the game object of the entity representing the entire
    game object hierarchy.

--- Packages/net.wesleybrown.sakura.bodies/Runtime/CollidableMovement/Data/Collisions/RecordCollidee.cs
+++ Packages/net.wesleybrown.sakura.bodies/Runtime/CollidableMovement/Data/Collisions/RecordCollidee.cs
@@ -2,13 +2,33 @@
 
 namespace Sakura.Bodies.CollidableMovement.Data
 {
+    /// <summary>
+    ///     A component that is necessary to allow
+    ///     <see cref="CharacterControllerCollisions"/>s to find the
+    ///     <see cref="GameObject"/> that a <see cref="CharacterController"/>
+    ///     collided with.
+    /// </summary>
+    /// <remarks>
+    ///     Since <see cref="CharacterController"/>s report the
+    ///     <see cref="GameObject"/>s they collide with through the
+    ///     <see cref="OnControllerColliderHit(ControllerColliderHit)"/>
+    ///     message, this component needs to be placed on any
+    ///     <see cref="GameObject"/> a
+    ///     <see cref="CharacterControllerCollisions"/> is looking at the
+    ///     collisions for before any <see cref="CharacterController"/> methods
+    ///     are called.
+    /// </remarks>
     sealed class RecordCollidee : MonoBehaviour
     {
+        /// <summary>
+        ///     The <see cref="GameObject"/> that a
+        ///     <see cref="CharacterController"/> collided with.
+        /// </summary>
         public GameObject Collidee { get; private set; }
 
         private void OnControllerColliderHit(ControllerColliderHit hit)
         {
-            Collidee = hit.collider.gameObject;
+            Collidee = hit.collider.transform.root.gameObject;
         }
     }
 }
