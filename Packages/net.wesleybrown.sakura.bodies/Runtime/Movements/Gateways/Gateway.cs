2d08f877fdb3748aabf9b469291a07e20db80749
Wesley Brown <wesleybrown@wesleybrown.net>
2022-04-09 12:53:48 -0400

    Create movement system gateway
    Creating a movement system gateway will not support a null dictionary of
    bodies.

---
+++ Packages/net.wesleybrown.sakura.bodies/Runtime/Movements/Gateways/Gateway.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Sakura.Bodies.Core;
+
+namespace Sakura.Bodies.Movements.Gateways
+{
+    /// <summary>
+    ///     A movement system gateway.
+    /// </summary>
+    internal sealed class Gateway
+    {
+        /// <summary>
+        ///     Create a movement system gateway made of a given dicitonary of
+        ///     bodies and dictionary of movements.
+        /// </summary>
+        /// <param name="bodies">
+        ///     The dictionary of bodies.
+        /// </param>
+        /// <param name="movements">
+        ///     The dictionary of movements.
+        /// </param>
+        /// <returns>
+        ///     A movement system gateway made of the given dictionary of
+        ///     bodies and movements.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the given dictionary of bodies is null.
+        /// </exception>
+        internal static Gateway Of(
+            IDictionary<Guid, Body> bodies,
+            IDictionary<Guid, Movement> movements)
+        {
+            if (bodies == null)
+                throw new ArgumentNullException(nameof(bodies));
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Packages/net.wesleybrown.sakura.bodies/Tests/Runtime/Movements/Gateways/GatewaySpec.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Sakura.Bodies.Core;
+using Sakura.Bodies.Movements.Gateways;
+
+namespace Movement_Gateway_Spec
+{
+    [TestFixture]
+    public class Creating
+    {
+        [Test]
+        public void With_A_Null_Dictionary_Of_Bodies_Is_Not_Supported()
+        {
+            IDictionary<Guid, Body> bodies = null;
+            var movements = new Dictionary<Guid, Movement>();
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                Gateway.Of(
+                    bodies,
+                    movements);
+            });
+        }
+    }
+}
